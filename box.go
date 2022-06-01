94cc7114fa5c7282d2dfc43397751d0b89238b21
Khrystyna2409 <xrustuna.di@gmail.com>
2022-06-01 14:09:44 +0300

    try

--- box.go
+++ box.go
@@ -26,6 +26,7 @@ func (b *box) AddShape(shape Shape) error {
 	if len(b.shapes) >= b.shapesCapacity {
 		err := errors.New("Something went wrong")
 		fmt.Println("Not good capacity:", err)
+		return err
 	}
 	b.shapes = append(b.shapes, shape)
 	return nil
@@ -36,8 +37,8 @@ func (b *box) AddShape(shape Shape) error {
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) GetByIndex(i int) (Shape, error) {
 	if i >= b.shapesCapacity {
-		err := errors.New("Something went wrong")
-		//fmt.Println("Not good capacity:", err)
+		err := errors.New("Value isn't existed")
+		fmt.Println("Not exist value:", err)
 		return nil, err
 	}
 	return b.shapes[i], nil
@@ -72,25 +73,31 @@ func (b *box) ReplaceByIndex(i int, shape Shape) (Shape, error) {
 
 // SumPerimeter provides sum perimeter of all shapes in the list.
 func (b *box) SumPerimeter() float64 {
-	var sum float64
+	var sump float64
 	for _, shape := range b.shapes {
-		sum += shape.CalcPerimeter()
-		fmt.Println(sum)
+		sump += shape.CalcPerimeter()
+		fmt.Println(sump)
 	}
-	return sum
+	return sump
 }
 
 // SumArea provides sum area of all shapes in the list.
 func (b *box) SumArea() float64 {
-	var sum float64
+	var suma float64
 	for _, shape := range b.shapes {
-		sum += shape.CalcArea()
+		suma += shape.CalcArea()
 	}
-	return sum
+	return suma
 }
 
 // RemoveAllCircles removes all circles in the list
 // whether circles are not exist in the list, then returns an error
 func (b *box) RemoveAllCircles() error {
-	return nil
+	//var list2 Circle
+	err := errors.New("don't know")
+	fmt.Println("how remove", err)
+	return err
+	//for k := range b.shapes {
+	//b.shapes = Remove(b.shapes, list2)
+
 }
